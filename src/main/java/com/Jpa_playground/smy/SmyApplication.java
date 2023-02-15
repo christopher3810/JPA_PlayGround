8d00b195ca83413e2d30c39b1dfc0e90fb0171c5
sungminyang <lonesome661@naver.com>
2023-02-16 08:07:46 +0900

    project init

---
+++ src/main/java/com/Jpa_playground/smy/SmyApplication.java
@@ -0,0 +1,13 @@
+package com.Jpa_playground.smy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Jpa_playground/smy/SmyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jpa_playground.smy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

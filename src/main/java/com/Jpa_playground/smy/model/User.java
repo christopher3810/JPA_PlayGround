6e2d95cb4d0ee90080d506661b8af3e4637687ac
sungminyang <lonesome661@naver.com>
2023-02-20 08:10:42 +0900

    add custom tsid supplier

--- src/main/java/com/Jpa_playground/smy/model/User.java
+++ src/main/java/com/Jpa_playground/smy/model/User.java
@@ -1,15 +1,26 @@
 package com.Jpa_playground.smy.model;
 
+import io.hypersistence.tsid.TSID;
 import io.hypersistence.utils.hibernate.id.Tsid;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
 
+import java.util.function.Supplier;
 @Entity(name="UserInfo")
 @Table(name="userInfo")
 public class User {
 
+    public static class CustomTsidSupplier implements Supplier<TSID.Factory>{
+        @Override
+        public TSID.Factory get(){
+            return TSID.Factory.builder().withNodeBits(1).build();
+        }
+    }
+
     @Id
-    @Tsid
+    @Tsid(CustomTsidSupplier.class)
     private long id;
 }

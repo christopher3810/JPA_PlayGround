886498e358d6088a289e7834716373ffeb15037d
sungminyang <lonesome661@naver.com>
2023-03-08 08:14:12 +0900

    cache demo code commit

---
+++ src/main/java/com/Jpa_playground/smy/CacheDemo/CachingITCompanyRepository.java
@@ -0,0 +1,15 @@
+package com.Jpa_playground.smy.CacheDemo;
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CachingITCompanyRepository extends CrudRepository<ITCompany, Long> {
+
+    @Override
+    @CacheEvict(value = "byCompanyName", key = "#p0.companyname")
+    <S extends ITCompany> S save(S entity);
+
+    @Cacheable("byCompanyName")
+    ITCompany findByCompanyName(String companyName);
+}
